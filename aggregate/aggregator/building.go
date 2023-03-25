a530008f4a426cebb8c2fd5e0163f6f28519b65f
swomfire <vucuongJim@gmail.com>
2023-03-25 22:48:26 +0700

    Enhance aggregate with generic

--- aggregate/aggregator/building.go
+++ aggregate/aggregator/building.go
@@ -1,8 +1,6 @@
 package aggregator
 
 import (
-	"fmt"
-
 	"github.com/R-jim/Momentum/aggregate/event"
 	"github.com/R-jim/Momentum/aggregate/store"
 	"github.com/google/uuid"
@@ -12,36 +10,18 @@ type BuildingState struct {
 	ID uuid.UUID
 }
 
-type buildingAggregateImpl struct {
-	store        *store.Store
-	aggregateSet map[event.Effect]func([]event.Event, event.Event) error
-}
-
 func NewBuildingAggregator(store *store.Store) Aggregator {
-	return mioAggregateImpl{
+	return aggregateImpl{
+		name:         "BUILDING",
 		store:        store,
 		aggregateSet: map[event.Effect]func([]event.Event, event.Event) error{},
 	}
 }
 
-func (i buildingAggregateImpl) GetStore() *store.Store {
-	return i.store
-}
-
-func (i buildingAggregateImpl) Aggregate(event event.Event) error {
-	if err := aggregate(i.store, i.aggregateSet, event); err != nil {
-		return fmt.Errorf("[BUILDING_AGGREGATE][%v] %v", event.Effect, err)
-	}
-	fmt.Printf("[BUILDING_AGGREGATE][%v] aggregated.\n", event.Effect)
-	return nil
-}
-
 func GetBuildingState(events []event.Event) (BuildingState, error) {
-	state := BuildingState{}
-
-	for _, e := range events {
+	return composeState(BuildingState{}, events, func(state BuildingState, e event.Event) (BuildingState, error) {
 		switch e.Effect {
 		}
-	}
-	return state, nil
+		return state, nil
+	})
 }
