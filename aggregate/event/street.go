14e84479d32fe6006311e3770e5e42e803db7d86
swomfire <vucuongJim@gmail.com>
2023-03-25 21:32:23 +0700

    Add street entity enter aggregate

--- aggregate/event/street.go
+++ aggregate/event/street.go
@@ -1,3 +1,19 @@
 package event
 
-const ()
+import (
+	"github.com/google/uuid"
+)
+
+const (
+	StreetEntityEnterEffect Effect = "STREET_ENTITY_ENTER"
+)
+
+func NewStreetEntityEnterEvent(streetID uuid.UUID, version int, entityID uuid.UUID) Event {
+	return Event{
+		ID:       uuid.New(),
+		EntityID: streetID,
+		Version:  version,
+		Effect:   StreetEntityEnterEffect,
+		Data:     entityID,
+	}
+}
