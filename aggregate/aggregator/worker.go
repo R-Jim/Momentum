bc3d1329cc23170ed571e50b168dca198e5f37a1
swomfire <vucuongJim@gmail.com>
2023-04-07 20:03:27 +0700

    Add building/worker assign/unassign

--- aggregate/aggregator/worker.go
+++ aggregate/aggregator/worker.go
@@ -7,20 +7,87 @@ import (
 )
 
 type WorkerState struct {
-	ID uuid.UUID
+	ID         uuid.UUID
+	BuildingID uuid.UUID
 }
 
 func NewWorkerAggregator(store *store.Store) Aggregator {
 	return aggregateImpl{
-		name:         "WORKER",
-		store:        store,
-		aggregateSet: map[event.Effect]func([]event.Event, event.Event) error{},
+		name:  "WORKER",
+		store: store,
+		aggregateSet: map[event.Effect]func([]event.Event, event.Event) error{
+			//"WORKER_INIT"
+			event.WorkerInitEffect: func(currentEvents []event.Event, inputEvent event.Event) error {
+				if len(currentEvents) != 0 {
+					return ErrAggregateFail
+				}
+
+				if inputEvent.EntityID.String() == uuid.Nil.String() {
+					return ErrAggregateFail
+				}
+
+				return nil
+			},
+			//"WORKER_ASSIGN"
+			event.WorkerAssignEffect: func(currentEvents []event.Event, inputEvent event.Event) error {
+				state, err := GetWorkerState(currentEvents)
+				if err != nil {
+					return err
+				}
+				if state.ID.String() == uuid.Nil.String() {
+					return ErrAggregateFail
+				}
+				if state.BuildingID.String() != uuid.Nil.String() {
+					return ErrAggregateFail
+				}
+
+				_, err = event.ParseData[uuid.UUID](inputEvent)
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+			//"WORKER_UNASSIGN"
+			event.WorkerUnassignEffect: func(currentEvents []event.Event, inputEvent event.Event) error {
+				state, err := GetWorkerState(currentEvents)
+				if err != nil {
+					return err
+				}
+				if state.ID.String() == uuid.Nil.String() {
+					return ErrAggregateFail
+				}
+				if state.BuildingID.String() == uuid.Nil.String() {
+					return ErrAggregateFail
+				}
+
+				buildingID, err := event.ParseData[uuid.UUID](inputEvent)
+				if err != nil {
+					return err
+				}
+
+				if state.BuildingID != buildingID {
+					return ErrAggregateFail
+				}
+
+				return nil
+			},
+		},
 	}
 }
 
 func GetWorkerState(events []event.Event) (WorkerState, error) {
 	return composeState(WorkerState{}, events, func(state WorkerState, e event.Event) (WorkerState, error) {
 		switch e.Effect {
+		case event.WorkerInitEffect:
+			state.ID = e.EntityID
+		case event.WorkerAssignEffect:
+			buildingID, err := event.ParseData[uuid.UUID](e)
+			if err != nil {
+				return state, err
+			}
+			state.BuildingID = buildingID
+		case event.WorkerUnassignEffect:
+			state.BuildingID = uuid.Nil
 		}
 		return state, nil
 	})
--- aggregate/event/worker.go
+++ aggregate/event/worker.go
@@ -1,3 +1,39 @@
 package event
 
-const ()
+import "github.com/google/uuid"
+
+const (
+	WorkerInitEffect Effect = "WORKER_INIT"
+
+	WorkerAssignEffect   Effect = "WORKER_ASSIGN"
+	WorkerUnassignEffect Effect = "WORKER_UNASSIGN"
+)
+
+func NewWorkerInitEvent(entityID uuid.UUID) Event {
+	return Event{
+		ID:       uuid.New(),
+		EntityID: entityID,
+		Version:  1,
+		Effect:   WorkerInitEffect,
+	}
+}
+
+func NewWorkerAssignEvent(entityID uuid.UUID, buildingID uuid.UUID, version int) Event {
+	return Event{
+		ID:       uuid.New(),
+		EntityID: entityID,
+		Version:  version,
+		Effect:   WorkerAssignEffect,
+		Data:     buildingID,
+	}
+}
+
+func NewWorkerUnassignEvent(entityID uuid.UUID, buildingID uuid.UUID, version int) Event {
+	return Event{
+		ID:       uuid.New(),
+		EntityID: entityID,
+		Version:  version,
+		Effect:   WorkerUnassignEffect,
+		Data:     buildingID,
+	}
+}
